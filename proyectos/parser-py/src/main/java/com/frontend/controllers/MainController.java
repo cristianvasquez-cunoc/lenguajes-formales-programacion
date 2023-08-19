ec9f51f8b5550da3ceabdfe14910bfe54054f00f
alejandro <alejandrovasquezesc@gmail.com>
2023-08-19 04:39:30 -0600

    added functionality to update line and column when textArea it's clicked or Key.

--- proyectos/parser-py/src/main/java/com/frontend/controllers/MainController.java
+++ proyectos/parser-py/src/main/java/com/frontend/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.frontend.controllers;
 
+import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 
@@ -13,15 +16,33 @@ public class MainController {
 
     @FXML
     TextArea textArea;
+    @FXML
+    Label lineCol;
 
     public void applyColorsToText(KeyEvent event) throws IOException {
 
-        textArea.setStyle("-fx--background--color: #ddd");
+        setLineCol();
 
         if (event.getCode().isLetterKey() || event.getCode().isDigitKey()) {
             String typedText = event.getText();
-            System.out.println("Typed character: " + typedText + ". Text in area: " + textArea.getText());
+            String fullText = textArea.getText();
+            System.out.println("Typed character: " + typedText + ". Text in area: " + fullText);
             // Apply your color changes here
         }
     }
+
+    public void setLineCol() {
+        int caretPosition = textArea.getCaretPosition();
+
+        int line = textArea.getText(0, caretPosition).split("\n").length;
+        int col = caretPosition - textArea.getText(0, caretPosition).lastIndexOf("\n");
+
+        lineCol.setText("Ln " + line + ", Col " + col);
+    }
+
+    public void setLineColOnClck (MouseEvent e) throws IOException{
+        setLineCol();
+    }
+
+
 }
